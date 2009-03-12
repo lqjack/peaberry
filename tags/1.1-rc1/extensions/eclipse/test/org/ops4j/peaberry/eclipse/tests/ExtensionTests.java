df2106a452a5a60a1e92cf485f76cad98a7fbcc1
mcculls <mcculls@c1daca8c-4d3a-0410-bdaa-d3da786970b0>
2009-03-12 07:38:47 +0000

    Tag 1.1-rc1

---
+++ tags/1.1-rc1/extensions/eclipse/test/org/ops4j/peaberry/eclipse/tests/ExtensionTests.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (C) 2009 Stuart McCulloch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ops4j.peaberry.eclipse.tests;
+
+import static com.google.inject.Guice.createInjector;
+import static org.ops4j.peaberry.Peaberry.osgiModule;
+import static org.ops4j.peaberry.eclipse.EclipseRegistry.eclipseRegistry;
+
+import org.ops4j.peaberry.Import;
+import org.ops4j.peaberry.ServiceRegistry;
+import org.ops4j.peaberry.eclipse.ExtensionInterface;
+import org.ops4j.peaberry.util.StaticImport;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.testng.annotations.Test;
+
+import com.google.inject.Inject;
+
+import examples.menu.Item;
+
+/**
+ * @author mcculls@gmail.com (Stuart McCulloch)
+ */
+@Test
+public final class ExtensionTests {
+
+  private static BundleContext BUNDLE_CONTEXT;
+
+  @Test(enabled = false)
+  public static void setBundle(final Bundle bundle) {
+    BUNDLE_CONTEXT = bundle.getBundleContext();
+  }
+
+  @ExtensionInterface("examples.menu.items")
+  private static interface ItemFacade {
+    String getLabel();
+  }
+
+  @Inject
+  ServiceRegistry registry;
+
+  public ExtensionTests() {
+    createInjector(osgiModule(BUNDLE_CONTEXT, eclipseRegistry())).injectMembers(this);
+  }
+
+  public void testExtensionPoint() {
+    registry.add(new StaticImport<Item>(new Item() {
+      public String getLabel() {
+        return "Service-based menu item";
+      }
+    }));
+
+    System.out.println("@ExtensionInterface proxy");
+    System.out.println("----------------------------------------------------------------");
+    for (final Import<ItemFacade> item : registry.lookup(ItemFacade.class, null)) {
+      System.out.println("getClass(): " + item.get().getClass());
+      System.out.println("toString(): " + item.get().toString());
+      System.out.println("getName():  " + item.get().getLabel());
+      System.out.println("equals():   " + item.get().equals(item.get()));
+      System.out.println("----------------------------------------------------------------");
+    }
+    System.out.println("\ncreateExecutableExtension");
+    System.out.println("----------------------------------------------------------------");
+    for (final Import<Item> item : registry.lookup(Item.class, null)) {
+      System.out.println("getClass(): " + item.get().getClass());
+      System.out.println("toString(): " + item.get().toString());
+      System.out.println("getName():  " + item.get().getLabel());
+      System.out.println("equals():   " + item.get().equals(item.get()));
+      System.out.println("----------------------------------------------------------------");
+    }
+    System.out.println();
+  }
+}
---
+++ tags/1.1-rc1/src/org/ops4j/peaberry/internal/ImportProxyClassLoader.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (C) 2008 Stuart McCulloch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ops4j.peaberry.internal;
+
+import static java.security.AccessController.doPrivileged;
+import static jsr166y.ConcurrentReferenceHashMap.ReferenceType.WEAK;
+import static org.ops4j.peaberry.internal.ComputedMapFactory.computedMap;
+import static org.ops4j.peaberry.internal.ImportGlue.generateProxy;
+import static org.ops4j.peaberry.internal.ImportGlue.getClazzName;
+import static org.ops4j.peaberry.internal.ImportGlue.getProxyName;
+
+import java.lang.reflect.Constructor;
+import java.security.PrivilegedAction;
+import java.util.concurrent.ConcurrentMap;
+
+import org.ops4j.peaberry.Import;
+import org.ops4j.peaberry.ServiceException;
+import org.ops4j.peaberry.ServiceUnavailableException;
+import org.ops4j.peaberry.internal.ComputedMapFactory.Function;
+
+/**
+ * Custom classloader that provides optimized proxies for imported services.
+ * 
+ * @author mcculls@gmail.com (Stuart McCulloch)
+ */
+final class ImportProxyClassLoader
+    extends ClassLoader {
+
+  private static final String UNAVAILABLE_CLAZZ_NAME = ServiceUnavailableException.class.getName();
+  private static final String IMPORT_CLAZZ_NAME = Import.class.getName();
+
+  @SuppressWarnings("unchecked")
+  static <T> Constructor<T> getProxyConstructor(final Class<T> clazz) {
+    try {
+      // use a different custom classloader for each class-space, to avoid leaks
+      final ClassLoader proxyLoader = getProxyClassLoader(clazz.getClassLoader());
+      final Class<?> proxyClazz = proxyLoader.loadClass(getProxyName(clazz.getName()));
+      return (Constructor<T>) proxyClazz.getConstructor(Import.class);
+    } catch (final LinkageError e) {
+      throw new ServiceException(e);
+    } catch (final NoSuchMethodException e) {
+      // /CLOVER:OFF
+      throw new ServiceException(e);
+    } catch (final ClassNotFoundException e) {
+      throw new ServiceException(e);
+      // /CLOVER:ON
+    }
+  }
+
+  // weak map of classloaders, to allow eager collection of proxied classes
+  private static final ConcurrentMap<ClassLoader, ClassLoader> LOADER_MAP =
+      computedMap(WEAK, WEAK, 32, new Function<ClassLoader, ClassLoader>() {
+        public ClassLoader compute(final ClassLoader parent) {
+          return doPrivileged(new PrivilegedAction<ClassLoader>() {
+            public ClassLoader run() {
+              return new ImportProxyClassLoader(parent);
+            }
+          });
+        }
+      });
+
+  private static ClassLoader getProxyClassLoader(final ClassLoader typeLoader) {
+    return LOADER_MAP.get(null == typeLoader ? getSystemClassLoader() : typeLoader);
+  }
+
+  // delegate to the original type's classloader
+  ImportProxyClassLoader(final ClassLoader parent) {
+    super(parent);
+  }
+
+  @Override
+  protected Class<?> loadClass(final String name, final boolean resolve)
+      throws ClassNotFoundException {
+
+    // short-circuit access to these classes
+    if (IMPORT_CLAZZ_NAME.equals(name)) {
+      return Import.class;
+    }
+    if (UNAVAILABLE_CLAZZ_NAME.equals(name)) {
+      return ServiceUnavailableException.class;
+    }
+
+    return super.loadClass(name, resolve);
+  }
+
+  @Override
+  protected Class<?> findClass(final String clazzOrProxyName)
+      throws ClassNotFoundException {
+
+    final String clazzName = getClazzName(clazzOrProxyName);
+
+    // is this a new proxy class request?
+    if (!clazzName.equals(clazzOrProxyName)) {
+      final byte[] code = generateProxy(loadClass(clazzName));
+      return defineClass(clazzOrProxyName, code, 0, code.length);
+    }
+
+    // ignore any non-proxy requests
+    throw new ClassNotFoundException(clazzOrProxyName);
+  }
+}
